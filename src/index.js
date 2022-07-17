3911d96e62379ab3802fd677e4ff95aebece7854
Weiss Luxford <weissluxford@gmail.com>
2022-07-17 19:14:35 +0500

    solved

--- src/index.js
+++ src/index.js
@@ -38,7 +38,23 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const decodedBits = expr.match(/(\d{10})|(\D{10})/gi)
+    const morse = decodedBits.reduce((acc, value) => {
+       let bitsDecoder = value.replace(/\D{10}/g, "").replace(/10/g, ".").replace(/11/g, "-").replace(/00/g, "")
+        return `${acc} ${bitsDecoder}`
+    }, "")
+    
+    const decodedWords = morse.split(' ').reduce((acc, value) => {
+        for(let [keys, values] of Object.entries(MORSE_TABLE)){
+            if (value === keys){
+                acc = `${acc}${values}`
+            } 
+        }
+        acc = value === "" ? `${acc} ` : acc
+        return acc
+    }, "")
+    console.log(decodedWords);
+    return decodedWords.trim()
 }
 
 module.exports = {
